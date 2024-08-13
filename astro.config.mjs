79603bd4404d3fcf7ab065389d942167789afa01
hobbeLe <20755338+tobiashaas@users.noreply.github.com>
2024-08-13 21:59:54 +0200

    logo black to dark...

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
 			defaultLocale: 'root',
 			logo: {
 				dark: './src/assets/acss-logo-white.svg',
-				light: './src/assets/automatic-css-logo-black.svg',				
+				light: './src/assets/automatic-css-logo-dark.svg',				
 				replacesTitle: true,
 			},
 			// Set English as the default language for this site.
